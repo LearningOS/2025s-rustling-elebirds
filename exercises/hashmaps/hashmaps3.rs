00f20c0241c5df4f12ffded018ab087f1b651ec7
elebirds <hhmcn@outlook.com>
2025-03-20 19:38:23 +0800

    3.21 初试rust

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,14 +14,19 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store the goal details of a team.
 struct Team {
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+impl Team {
+    fn add(&mut self, goals_scored: u8, goals_conceded: u8) {
+        self.goals_scored += goals_scored;
+        self.goals_conceded += goals_conceded;
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -39,6 +44,12 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        scores.entry(team_1_name.to_owned()).and_modify(|team_1| {
+            team_1.add(team_1_score, team_2_score);
+        }).or_insert(Team { goals_scored: team_1_score, goals_conceded: team_2_score });
+        scores.entry(team_2_name.to_owned()).and_modify(|team_2| {
+            team_2.add(team_2_score, team_1_score);
+        }).or_insert(Team { goals_scored: team_2_score, goals_conceded: team_1_score });
     }
     scores
 }
